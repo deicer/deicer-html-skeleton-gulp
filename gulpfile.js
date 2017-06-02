6ededaa2a2c1d2d94ce4494718613f529c9d2fbb
deicer <gena-lebedev@yandex.ru>
2017-06-03 00:20:05 +0700

    add some plugins in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 'use strict';
+
+/*
+ * 
+ * Подключаем gulp, его плагины и другие модули 
+ *
+ */
+
+ var gulp            = require('gulp'),
+     argv            = require('yargs').argv, // Плагин для разбора опция командной строки
+     path	         = require('path'),       // Модуль Node для работы с файловыми путями 
+     gulpif          = require('gulp-if'),    // Управление потоками выполнения gulp
+     

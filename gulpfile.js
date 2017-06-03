a5a8eb88ddb26ab8af79172159f1dbc515bb9458
gl <g.lebedev@auchan.ru>
2017-06-03 17:57:01 +0700

    add some plugins to gulpfule.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 'use strict';
 
-/*
- * 
- * Подключаем gulp, его плагины и другие модули 
- *
- */
+/* ===== ПОДКЛЮЧЕНИЕ ПЛАГИНОВ ===== */
 
- var gulp            = require('gulp'),
-     argv            = require('yargs').argv, // Плагин для разбора опция командной строки
-     path	         = require('path'),       // Модуль Node для работы с файловыми путями 
-     gulpif          = require('gulp-if'),    // Управление потоками выполнения gulp
-     
+var gulp        = require('gulp'),               // gulp
+    argv        = require('yargs').argv,         // Разбор опций командной строки gulp
+    path        = require('path'),               // Модуль Node для работы с файловыми путями
+    gulpif      = require('gulp-if'),            // Управление потоками выполнения gulp
+    watch       = require('gulp-watch'),         // Отслеживание изменений в файлах проекта
+    uglify      = require('gulp-uglify'),        // Минификация JavaScript
+    pug         = require('gulp-pug'),           // Шаблонизатор pug (jade)
+    sass	    = require('gulp-sass'),          // Препроцессор SASS
+    less        = require('gulp-less'),          // Препроцессор LESS
+    rigger	    = require('gulp-rigger'),
